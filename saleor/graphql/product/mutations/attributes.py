381bddfbc13983ee5c5366334c934afa9a4dc28e
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-09-27 16:50:47 +0200

    Refactor GraphQL Attributes mutation

--- saleor/graphql/product/mutations/attributes.py
+++ saleor/graphql/product/mutations/attributes.py
@@ -1,33 +1,23 @@
 import graphene
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import slugify
 
 from ....product import models
 from ...core.mutations import ModelDeleteMutation, ModelMutation
-from django.core.exceptions import ValidationError
 
 
-class AttributeValueCreateInput(graphene.InputObjectType):
-    attribute = graphene.ID(
-        required=False,
-        description='Attribute to which value will be assigned.',
-        name='attribute')
-    slug = graphene.String(
-        required=True, description='Internal name.')
+class AttributeCreateValueInput(graphene.InputObjectType):
     name = graphene.String(
         required=True, description='Name displayed in the interface.')
     value = graphene.String(
         required=True, description='Real value eg. HEX color.')
 
 
-class AttributeInput(graphene.InputObjectType):
-    slug = graphene.String(
-        required=True, description='Internal name.')
+class AttributeCreateInput(graphene.InputObjectType):
     name = graphene.String(
         required=True, description='Name displayed in the interface.')
-
-
-class AttributeCreateInput(AttributeInput):
     values = graphene.List(
-        AttributeValueCreateInput,
+        AttributeCreateValueInput,
         description='Attribute values to be created for this attribute.')
 
 
@@ -48,6 +38,7 @@ class AttributeCreate(ModelMutation):
     @classmethod
     def clean_input(cls, info, instance, input, errors):
         cleaned_input = super().clean_input(info, instance, input, errors)
+        cleaned_input['slug'] = slugify(cleaned_input['name'])
 
         values = cleaned_input.get('values', [])
         names = [value['name'] for value in values]
@@ -55,14 +46,13 @@ class AttributeCreate(ModelMutation):
             cls.add_error(
                 errors, 'values', 'Duplicated attribute value names provided.')
         for value_data in values:
+            value_data['slug'] = slugify(value_data['name'])
             attribute_value = models.AttributeValue(**value_data)
             try:
                 attribute_value.full_clean()
             except ValidationError as validation_errors:
                 for field in validation_errors.message_dict:
                     # Attribute instance is not created yet so we cannot add it
-                    if field == 'attribute':
-                        continue
                     for message in validation_errors.message_dict[field]:
                         error_field = 'values:%(field)s' % {'field': field}
                         cls.add_error(errors, error_field, message)
@@ -76,11 +66,24 @@ class AttributeCreate(ModelMutation):
             instance.values.create(**value)
 
 
+class AttributeUpdateInput(graphene.InputObjectType):
+    slug = graphene.String(
+        required=True, description='Internal name.')
+    name = graphene.String(
+        required=True, description='Name displayed in the interface.')
+    remove_values = graphene.List(
+        graphene.ID, name='removeValues',
+        description='List of attributes to be removed from this attribute.')
+    add_values = graphene.List(
+        AttributeCreateValueInput, name='addValues',
+        description='Attribute values to be created for this attribute.')
+
+
 class AttributeUpdate(AttributeCreate):
     class Arguments:
         id = graphene.ID(
             required=True, description='ID of an attribute to update.')
-        input = AttributeInput(
+        input = AttributeUpdateInput(
             required=True,
             description='Fields required to update an attribute.')
 
@@ -103,6 +106,17 @@ class AttributeDelete(ModelDeleteMutation):
         return user.has_perm('product.manage_products')
 
 
+class AttributeValueCreateInput(graphene.InputObjectType):
+    attribute = graphene.ID(
+        required=False,
+        description='Attribute to which value will be assigned.',
+        name='attribute')
+    name = graphene.String(
+        required=True, description='Name displayed in the interface.')
+    value = graphene.String(
+        required=True, description='Real value eg. HEX color.')
+
+
 class AttributeValueCreate(ModelMutation):
     class Arguments:
         input = AttributeValueCreateInput(
@@ -117,13 +131,28 @@ class AttributeValueCreate(ModelMutation):
     def user_is_allowed(cls, user, input):
         return user.has_perm('product.manage_products')
 
+    @classmethod
+    def clean_input(cls, info, instance, input, errors):
+        cleaned_input = super().clean_input(info, instance, input, errors)
+        cleaned_input['slug'] = slugify(cleaned_input['name'])
+        return cleaned_input
+
+
+class AttributeValueUpdateInput(graphene.InputObjectType):
+    slug = graphene.String(
+        required=True, description='Internal name.')
+    name = graphene.String(
+        required=True, description='Name displayed in the interface.')
+    value = graphene.String(
+        required=True, description='Real value eg. HEX color.')
+
 
 class AttributeValueUpdate(AttributeValueCreate):
     class Arguments:
         id = graphene.ID(
             required=True,
             description='ID of an attribute choice value to update.')
-        input = AttributeInput(
+        input = AttributeValueUpdateInput(
             required=True,
             description='Fields required to update an attribute choice value.')
 
